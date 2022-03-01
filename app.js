a1b2d93da3eace3d40a29f0a70ae3e2340e6bca1
nurmdrafi <nurmdrafi2014@gmail.com>
2022-03-01 22:14:48 +0600

    spinner added

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 // Global Variables
 const phoneContainer = document.getElementById('phones-container');
+const spinner = document.getElementById('spinner');
 
 // Load Phone Data from API
 const loadPhones = () => {
-  const searchText = document.getElementById("search-box").value;
-  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayPhones(data.data));
+const searchField = document.getElementById("search-box");
+const searchText = searchField.value;
+const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+fetch(url)
+.then((res) => res.json())
+.then((data) => displayPhones(data.data));
+spinner.style.display = "block";
+searchField.value = "";
 };
 
 // Display Phones
 const displayPhones = (phones) => {
-  phones.forEach(phone =>{
-      const div = document.createElement('div');
-      div.classList.add('col');
-      div.innerHTML = `
-                    <div class="card">
-                        <img src="${phone.image}" alt="" class="card-img-top w-75 mx-auto">
-                        <div class="card-body text-center">
-                            <h3 class="card-title">${phone.phone_name}</h3>
-                            <h5>${phone.brand}</h5>
-                            <button class="btn btn-primary" onclick="loadDetails('${phone.slug}')">Details</button>
-                        </div>
-                    </div>
-      `;
-      phoneContainer.appendChild(div);
-  })
+    if (phones.length === 0) {
+        spinner.innerText = 'No result found';
+        phoneContainer.textContent = "";
+    } else{
+        phoneContainer.textContent = "";
+        spinner.innerText = 'Loading...'
+        phones.forEach(phone =>{
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                            <div class="card">
+                                <img src="${phone.image}" alt="" class="card-img-top w-75 mx-auto">
+                                <div class="card-body text-center">
+                                    <h3 class="card-title">${phone.phone_name}</h3>
+                                    <h5>${phone.brand}</h5>
+                                    <button class="btn btn-primary" onclick="loadDetails('${phone.slug}')">Details</button>
+                                </div>
+                            </div>
+            `;
+            phoneContainer.appendChild(div);
+      })
+      spinner.style.display = "none";
+    }
+      
 };
 
 // Load Phone Details
@@ -43,8 +56,8 @@ const displayDetails = (details) =>{
     phoneDetails.textContent = "";
     const div = document.createElement('div');
     div.classList.add('w-50', 'mx-auto');
-    
-    const mainFeature = `
+
+    const mainFeatures = `
     <h2>${details.name}</h2>
     <p class="text-muted">${details.releaseDate ? details.releaseDate : "No release date found"}</p><br>
     <p class="text-danger fw-bold">Main Features</p>
@@ -54,11 +67,11 @@ const displayDetails = (details) =>{
     <p><span class="fw-bold">Sensors:</span> ${details.mainFeatures.sensors.join(", ")}</p><br>
     `;
     if(!details.others){
-        div.innerHTML = `${mainFeature}`
+        div.innerHTML = `${mainFeatures}`
 
     } else if(details.others){
         div.innerHTML = `
-        ${mainFeature}
+        ${mainFeatures}
         <p class="text-danger fw-bold">Other Features</p>
         <p><span class="fw-bold">Bluetooth:</span> ${details.others.Bluetooth}</p>
         <p><span class="fw-bold">GPS:</span> ${details.GPS}</p>
