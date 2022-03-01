9b302c3287dbb850aebd23e1b8dd6eb3d0da17e1
nurmdrafi <nurmdrafi2014@gmail.com>
2022-03-01 20:14:57 +0600

    display phones

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 // Load Data from API
 
-const loadPhones = () =>{
-    const searchText = document.getElementById('search-box').value;
-    console.log(searchText);
-}
+// Global Variables
+const phoneContainer = document.getElementById('phones-container');
+
+const loadPhones = () => {
+  const searchText = document.getElementById("search-box").value;
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayPhones(data.data));
+};
+
+const displayPhones = (phones) => {
+  phones.forEach(phone =>{
+      const div = document.createElement('div');
+      div.classList.add('col');
+      console.log(div);
+      div.innerHTML = `
+                    <div class="card">
+                        <img src="${phone.image}" alt="" class="card-img-top w-75 mx-auto">
+                        <div class="card-body text-center">
+                            <h3 class="card-title">${phone.phone_name}</h3>
+                            <h5>${phone.brand}</h5>
+                            <button classs="btn btn-primary" onclick="">Details</button>
+                        </div>
+                    </div>
+      `;
+      phoneContainer.appendChild(div);
+  })
+};
