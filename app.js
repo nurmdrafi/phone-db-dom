204ed6f1d3093fa2ba2ab793ac4878dfa57be4b9
nurmdrafi <nurmdrafi2014@gmail.com>
2022-03-01 21:26:45 +0600

    others features added

--- app.js
+++ app.js
@@ -42,22 +42,37 @@ const displayDetails = (details) =>{
     const phoneDetails = document.getElementById('phone-details');
     phoneDetails.textContent = "";
     const div = document.createElement('div');
-    div.classList.add('w-50', 'mx-auto')
+    div.classList.add('w-50', 'mx-auto');
+    if(!details.others){
     div.innerHTML = `
         <h2>${details.name}</h2>
-        <p>${details.releaseDate}</p><br>
-        <p>Main Features</p>
-        <p>ChipSet: ${details.mainFeatures.chipSet}</p>
-        <p>Display Size: ${details.mainFeatures.displaySize}</p>
-        <p>Memory: ${details.mainFeatures.memory}</p>
-        <p>Sensors: ${details.mainFeatures.sensors.join(", ")}</p><br>
-        <p>Other Features</p>
-        <p>Bluetooth: ${details.others.Bluetooth}</p>
-        <p>GPS: ${details.GPS}</p>
-        <p>NFC: ${details.others.NFC}</p>
-        <p>Radio: ${details.others.Radio}</p>
-        <p>USB: ${details.USB}</p>
-        <p>WLAN: ${details.others.WLAN}</p>
-    `;
+        <p class="text-muted">${details.releaseDate ? details.releaseDate : "No release date found"}</p><br>
+        <p class="text-danger fw-bold">Main Features</p>
+        <p><span class="fw-bold">ChipSet:</span> ${details.mainFeatures.chipSet}</p>
+        <p><span class="fw-bold">Display Size:</span> ${details.mainFeatures.displaySize}</p>
+        <p><span class="fw-bold">Memory:</span> ${details.mainFeatures.memory}</p>
+        <p><span class="fw-bold">Sensors:</span> ${details.mainFeatures.sensors.join(", ")}</p><br>
+        `;
+
+    } else if(details.others){
+        div.innerHTML = `
+        <h2>${details.name}</h2>
+        <p class="text-muted">${details.releaseDate ? details.releaseDate : "No release date found"}</p><br>
+        <p class="text-danger fw-bold">Main Features</p>
+        <p><span class="fw-bold">ChipSet:</span> ${details.mainFeatures.chipSet}</p>
+        <p><span class="fw-bold">Display Size:</span> ${details.mainFeatures.displaySize}</p>
+        <p><span class="fw-bold">Memory:</span> ${details.mainFeatures.memory}</p>
+        <p><span class="fw-bold">Sensors:</span> ${details.mainFeatures.sensors.join(", ")}</p><br>
+        <p class="text-danger fw-bold">Other Features</p>
+        <p><span class="fw-bold">Bluetooth:</span> ${details.others.Bluetooth}</p>
+        <p><span class="fw-bold">GPS:</span> ${details.GPS}</p>
+        <p><span class="fw-bold">NFC:</span> ${details.others.NFC}</p>
+        <p><span class="fw-bold">Radio:</span> ${details.others.Radio}</p>
+        <p><span class="fw-bold">USB:</span> ${details.USB}</p>
+        <p><span class="fw-bold">WLAN:</span> ${details.others.WLAN}</p>
+        `;
+    }
     phoneDetails.appendChild(div);
+    
+
 }
